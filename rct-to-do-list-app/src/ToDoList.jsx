862d928c2be3fb649edfd58d82813c3760292457
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-28 00:02:47 +0530

    9. moveTasksDown⬇️

--- rct-to-do-list-app/src/ToDoList.jsx
+++ rct-to-do-list-app/src/ToDoList.jsx
@@ -35,7 +35,11 @@ function ToDoList() {
         }
    }
    function moveTaskDown(index) {
-
+        if (index < tasks.length - 1) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index],updatedTasks[index + 1]] = [updatedTasks[index + 1],updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
    }
     return(
         <div className="to-do-list" >
